e244b1808d6a197b0b84bf26927cc844ff80fd2e
Kamile Pimenta <kamile.pimenta@outlook.com>
2024-10-27 15:59:20 -0300

    feat: prepare custom web component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { createCustomElement } from '@angular/elements';
+import { OrderComponent } from './pages/order/order.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OrderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'web-components-order';
+
+  constructor( private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const childAppComponent = createCustomElement(AppComponent, {  injector: this.injector });
+    customElements.define('kms-order', childAppComponent);
+  }
 }
